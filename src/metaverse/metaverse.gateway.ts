1c1ef253ef2a8c4a5e0b90343fa04d88143071f2
songhs0718@gmail.com <songhs0718@gmail.com>
2024-01-24 15:46:32 +0900

    metaverse v0.1

--- src/metaverse/metaverse.gateway.ts
+++ src/metaverse/metaverse.gateway.ts
@@ -6,26 +6,43 @@ export class MetaverseGateway implements OnGatewayConnection, OnGatewayDisconnec
 
   @WebSocketServer()
   server: Server;
-
-  users: { [user_name: string]: {}} = {}
+  
+  users: { [user_name: string]: {}} = {};
+  clientUserName: {[socketId: string]: string} = {};
 
   handleConnection(client: Socket): void {
-    console.log('connect')
+    this.server.to(client.id).emit('getUsers',this.users);
   }
 
   handleDisconnect(client: Socket):void {
-    console.log('disconnect')
+    console.log('disconnect',client.id)
+    client.leave('metaverse');
+    const user_name = this.clientUserName[client.id];
+    delete this.clientUserName[client.id];
+    delete this.users[user_name];
+    this.server.to('metaverse').emit('exitRoom',user_name);
   }
 
   @SubscribeMessage('join')
   join(client: Socket,user_name: string,position: {}): void {
       this.users[user_name] = position;
+      this.clientUserName[client.id] = user_name;
+      console.log(client.id);
       client.join('metaverse');
+      this.server.to('metaverse').emit('joinRoom',{ user_name, position });
   }
 
   @SubscribeMessage('exit')
   exit(client: Socket,user_name: string): void {
       client.leave('metaverse');
+      delete this.clientUserName[client.id];
       delete this.users[user_name];
+      this.server.to('metaverse').emit('exitRoom',user_name);
+      console.log(client.id);
+  }
+
+  @SubscribeMessage('move')
+  move(client: Socket,user_name: string,position: {}): void {
+    this.server.to('metaverse').emit('move',{ user_name, position });
   }
 }
