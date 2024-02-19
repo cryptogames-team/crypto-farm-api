79366c085fce196041348dcdd05ab06a3c30f3b6
songhs0718@gmail.com <songhs0718@gmail.com>
2024-02-19 17:31:14 +0900

    cook3

--- src/cook/cook.service.ts
+++ src/cook/cook.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { ItemRepository } from 'src/item/repositories/item.repository';
 import { OwnItemRepository } from 'src/item/repositories/own_item.repository';
 import { UserRepository } from 'src/user/repositories/user.repository';
@@ -17,27 +17,23 @@ export class CookService {
     ){}
 
     async cook(user: User, cookDto: cookDto){
-        const queryRunner = this.dataSource.createQueryRunner();
-        await queryRunner.connect();
-        await queryRunner.startTransaction();
         const { cook, ingredient1, ingredient2, item_index } = cookDto;
         const ingredient1_item = await this.itemRepository.getItemByID(ingredient1);
         const ingredient2_item = await this.itemRepository.getItemByID(ingredient2);
+        const found1 = this.ownItemRepository.getItemByUserIDANDItemID(user.user_id,ingredient1_item);
+        const found2 = this.ownItemRepository.getItemByUserIDANDItemID(user.user_id,ingredient2_item);
+        if(!found1 || !found2){
+            throw new NotFoundException('재료 부족')
+        }
+        
         const cook_item = await this.itemRepository.getItemByID(cook);
         const useItemDto1 = {item_id : ingredient1, item_count : 1};
         const useItemDto2 = {item_id : ingredient2, item_count : 1};
         const addItemDto = {item_id : cook, item_count : 1, item_index};
-        try {
-            await this.ownItemRepository.useItem(user,useItemDto1,ingredient1_item);
-            await this.ownItemRepository.useItem(user,useItemDto2,ingredient2_item);
-            await this.ownItemRepository.addItem(user,addItemDto,cook_item);
-            await queryRunner.commitTransaction();
-            return 'success';
-        }catch(error){
-            await queryRunner.rollbackTransaction();
-        }finally {
-            await queryRunner.release();
-        }
+        await this.ownItemRepository.useItem(user,useItemDto1,ingredient1_item);
+        await this.ownItemRepository.useItem(user,useItemDto2,ingredient2_item);
+        return await this.ownItemRepository.addItem(user,addItemDto,cook_item);
+
         
     }
 }
